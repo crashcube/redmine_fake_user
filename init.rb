5835ae316c0bac748d32ef17fa353fd15a217155
Denis Lysenko <dlysenko@me.com>
2013-12-05 17:31:20 +0400

    Init

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_fake_user do
+  name 'Redmine Fake User plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
